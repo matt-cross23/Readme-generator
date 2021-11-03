fc9ef1081203bf6613d9de41348a263a1de90b5f
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2021-11-03 15:07:40 -0400

    Finish Markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -6,6 +6,18 @@ function renderLicenseBadge(license) {
     case 'Apache License 2.0':
       return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
     // cases for other options
+    case "BSD 3-Clause License":
+      return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+      case "BSD 2-Clause License":
+      return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)"
+    case "GNU GPL v3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+      case "MIT license":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+    case "Mozilla Public License 2.0":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+      case "Eclipse Public License version 1.0"
+      return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
     default:
       return ""
     }
@@ -17,6 +29,18 @@ function renderLicenseLink(license) {
   switch(license){
     case 'Apache License 2.0':
 return '(https://opensource.org/licenses/Apache-2.0)'
+    case "BSD 3-Clause License":
+      return "(https://opensource.org/licenses/BSD-3-Clause)"
+      case "BSD 2-Clause License":
+      return "(https://opensource.org/licenses/BSD-2-Clause)"
+    case "GNU GPL v3":
+      return "(https://www.gnu.org/licenses/gpl-3.0)"
+      case "MIT license":
+      return "(https://opensource.org/licenses/MIT)"
+    case "Mozilla Public License 2.0":
+      return "(https://opensource.org/licenses/MPL-2.0)"
+      case "Eclipse Public License version 1.0"
+      return "(https://opensource.org/licenses/EPL-1.0)"
 default: 
 return ""
   }
@@ -28,8 +52,18 @@ function renderLicenseSection(license) {
   switch(license){
     case 'Apache License 2.0':
     return "License"
-    case '':
-
+    case 'BSD 3-Clause license':
+    return "License"
+      case "BSD 2-Clause License":
+      return "Licenses"
+    case "GNU GPL v3":
+      return "License"
+      case "MIT license":
+      return "License"
+    case "Mozilla Public License 2.0":
+      return "License"
+      case "Eclipse Public License version 1.0"
+      return "License"
     default:
       return ""
   }
